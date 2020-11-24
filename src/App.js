366dc5b8d9937b1e4830958cffe3b86e24d5df99
dave <rladuddnr486@naver.com>
2020-11-24 09:16:38 +0900

    한명의 customer component 구조화

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React,{Component} from 'react';
-
+import Customer from './components/Customer.js'
 /*
 function App() {
   return (
@@ -24,13 +24,27 @@ function App() {
   );
 }
 */
+
+const customer = {
+  'id' :1,
+  'image' : 'https://placeimg.com/128/128/any',
+  'name': '홍길동',
+  'birthday' : '001010',
+  'gender' : '남자',
+  'job': '대학생'
+}
+
 class App extends Component{
   render(){
     return(
-      <div className ="gray-background">
-        <img src={logo} lat ="logo"/>
-        <h2>Let's develop management system!</h2>
-      </div>
+      <Customer 
+        id={customer.id}
+        image={customer.image}
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
     );
   }
 }
